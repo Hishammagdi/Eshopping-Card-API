9e3f8a0b1d373f383fd35ca1676d9c0a87376726
hisham_magdi <heshamelsisi99@gmail.com>
2025-06-22 03:01:45 +0300

    Add files via upload

---
+++ BusinessLogicLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLogicLayer.Interfaces;
+using BusinessLogicLayer.Specification;
+using DataAccessLayer.Data;
+using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationStoreDbContext _context;
+
+        public GenericRepository(ApplicationStoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+            => await _context.Set<T>().ToListAsync();
+        public async Task<T> GetByIdAsync(int id)
+            => await _context.Set<T>().FindAsync(id);
+
+        public async Task<IReadOnlyList<T>> GetAllWithSpecAsync(ISpecification<T> spec)
+            => await ApplySpecifications(spec).ToListAsync();
+
+        public async Task<T> GetByIdWithSpecAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecifications(spec).FirstOrDefaultAsync();
+        }
+
+
+        private IQueryable<T> ApplySpecifications(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>(), spec);
+        }
+        // To Get Count Product in API
+        public async Task<int> GetCountWithSpecAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecifications(spec).CountAsync();
+        }
+
+
+        public void Add(T entity)
+           => _context.Set<T>().Add(entity);
+        public void Update(T entity)
+            => _context.Set<T>().Update(entity);
+        public void Delete(T entity)
+            => _context.Set<T>().Remove(entity);
+    }
+}
---
+++ DataAccessLayer/Data/ApplicationStoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.Entities.Order;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data
+{
+    public class ApplicationStoreDbContext : DbContext
+    {
+        public ApplicationStoreDbContext(DbContextOptions<ApplicationStoreDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
